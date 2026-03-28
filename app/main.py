6ecce0ecac9a24187bdac236f5329b5afda1e54e
Victor Velma <x@Victors-MacBook-Pro.local>
2026-03-28 18:54:38 -0400

    creating basic base.html template and connecting fastapi

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from dev-log-portfolio!")
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "message": "Hello From Victor Spicter"}
+    )
+
